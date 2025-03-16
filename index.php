24e593c385431bdc205476b3bc768db1c25d397e
Samuel3853 <welski71@gmail.com>
2025-03-16 20:56:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>  
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-md navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto text-center">
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Publications</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">CV</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Header Section -->
+<header class="container-fluid bg1 text-center">
+    <div class="container tag">
+        <h1>Games Design For the Future</h1>
+    </div>
+</header>
+
+<!-- About Me Section -->
+<section class="container-fluid aboutbg text-center py-5">
+    <div class="container">
+        <h2>GAMES & LEVEL DESIGNER</h2>
+        <p class="particle">and particle wizard</p>
+        <p class="about-text">My name is Josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level Design, Scripting, and Visual Effects...</p>
+    </div><!-- container-->
+    <div class="blocker"></div>
+</section>
+
+<!-- Latest Articles Section -->
+<section class="container-fluid articlesbg py-5">
+    <div class="container">
+        <h3 class="text-center">Latest Articles</h3>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-12 text-center mb-4">
+                <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg" alt="portrait of Josh Whitkin">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">12 December 2020</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation...</p>
+                <a class="readmore" href="#">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4 col-sm-6 col-12 text-center mb-4">
+                <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="game environment">
+                <h4 class="article-title">GAME ENVIRONMENTS</h4>
+                <p class="date">5 November 2020</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation...</p>
+                <a class="readmore" href="#">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4 col-sm-12 col-12 text-center mb-4">
+                <img class="img-fluid" src="http://170.187.231.66/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="scripting techniques">
+                <h4 class="article-title">SCRIPTING TECHNIQUES</h4>
+                <p class="date">22 October 2020</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation...</p>
+                <a class="readmore" href="#">CONTINUE READING</a>
+            </div>
+        </div><!-- row-->
+    </div><!-- container-->
+</section>
+
+<!-- Footer Section -->
+<footer class="container-fluid text-center py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-2">
+                <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+            </div>
+            <div class="col-md-6">
+                <p><a href="#">Terms and Conditions</a></p>
+            </div>
+        </div>
+    </div><!-- container-->
+</footer>
+
+<!-- Script Imports -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<?php get_footer(); ?>  
