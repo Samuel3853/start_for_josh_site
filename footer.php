d019d8d3ec2f3e3be494dad40d2c44aa8092ca80
Samuel3853 <welski71@gmail.com>
2025-03-16 20:56:23 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+    <!-- Footer -->
+    <footer class="container-fluid text-center py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mb-2">
+                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+                </div>
+                <div class="col-md-6">
+                    <p><a href="#">Terms and Conditions</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+<?php wp_footer(); ?>
+ 
+</body>
+</html>
